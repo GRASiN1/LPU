e641c2ccfc16e2e01a1e02c9d0b1f70246781ef1
GRASiN1 <gauravrajsingh047@gmail.com>
2023-11-03 23:57:03 +0530

    4nov2023

--- DSA/05_Trees/04_BST_Search.cpp
+++ DSA/05_Trees/04_BST_Search.cpp
@@ -1,125 +1,92 @@
-// #include <iostream>
-// using namespace std;
-
-// class Node{
-//     public:
-//     int data;
-//     int key;
-//     Node* left;
-//     Node* right;
-
-//     Node(int val){
-//         data = val;
-//         left = NULL;
-//         right = NULL;
-//     }
-
-// };
-
-// Node* searchInBST(Node* root, int key){
-//     if(root == NULL) return NULL;
-
-//     if(root->data == key) return root;
-
-//     if(root->data > key){
-//         return searchInBST(root->left, key);
-//     }
-
-//     return searchInBST(root->right, key);
-
-// }
-
-// Node* inorderSucc(Node* root){
-//     Node* curr = root;
-//     while(curr && curr->left != NULL){
-//         curr = curr->left;
-//     }
-
-//     return curr;
-// }
-
-// Node* deleteInBST(Node* root, int key){
-
-//     if(key < root->data){
-//         root->left = deleteInBST(root->left, key);
-//     }
-
-//     else if(key > root->data){
-//         root->right = deleteInBST(root->right, key);
-//     }
-
-//     else{
-
-//         if(root->left == NULL){
-//             Node* temp = root->right;
-//             delete root;
-//             return temp;
-//         }
-
-//         else if(root->right == NULL){
-//             Node* temp = root->left;
-//             delete root;
-//             return temp;
-//         }
-
-//         //case 3
-//         Node* temp = inorderSucc(root->right);
-//         root->data = temp->data;
-//         root->right = deleteInBST(root->right, temp->key);
-//     }
-
-//     return root;
-
-// }
-
-// int main(){
-
-//     Node* root = new Node(4);
-//     root->left = new Node(2);
-//     root->right = new Node(5);
-
-//     root->left->left = new Node(1);
-//     root->left->right = new Node(3);
-//     root->right->right = new Node(6);
-
-//     if(searchInBST(root, 15) == NULL){
-//         cout << "Key doesn't exist";
-//     }
-//     else{
-//         cout << "key exist";
-//     }
-
-// }
-
 #include <iostream>
-#include <cstring>
+using namespace std;
 
-class MyString {
-public:
-  char* str;
+class Node{
+    public:
+    int data;
+    int key;
+    Node* left;
+    Node* right;
 
-  MyString(const char* s) {
-    str = new char[strlen(s) + 1];
-    strcpy(str, s);
-  }
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
 
-  MyString(const MyString& other) {
-    str = new char[strlen(other.str) + 1];
-    strcpy(str, other.str);
-  }
-
-  ~MyString() {
-    delete[] str;
-  }
 };
 
+Node* searchInBST(Node* root, int key){
+    if(root == NULL) return NULL;
 
-int main() {
-  MyString str1("Hello");
-  MyString str2 = str1;
+    if(root->data == key) return root;
 
-  str1.str[0] = 'C';
-  std::cout << str2.str << std::endl;
+    if(root->data > key){
+        return searchInBST(root->left, key);
+    }
 
-  return 0;
+    return searchInBST(root->right, key);
+
+}
+
+Node* inorderSucc(Node* root){
+    Node* curr = root;
+    while(curr && curr->left != NULL){
+        curr = curr->left;
+    }
+
+    return curr;
+}
+
+Node* deleteInBST(Node* root, int key){
+
+    if(key < root->data){
+        root->left = deleteInBST(root->left, key);
+    }
+
+    else if(key > root->data){
+        root->right = deleteInBST(root->right, key);
+    }
+
+    else{
+
+        if(root->left == NULL){
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        else if(root->right == NULL){
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        //case 3
+        Node* temp = inorderSucc(root->right);
+        root->data = temp->data;
+        root->right = deleteInBST(root->right, temp->key);
+    }
+
+    return root;
+
+}
+
+int main(){
+
+    Node* root = new Node(4);
+    root->left = new Node(2);
+    root->right = new Node(5);
+
+    root->left->left = new Node(1);
+    root->left->right = new Node(3);
+    root->right->right = new Node(6);
+
+    if(searchInBST(root, 15) == NULL){
+        cout << "Key doesn't exist";
+    }
+    else{
+        cout << "key exist";
+    }
+
 }
